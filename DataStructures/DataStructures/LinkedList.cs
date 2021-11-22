4589ef9509e9dc13812e3dc005693e257d0041b2
asc303 <chauhansam94@gmail.com>
2021-11-22 16:18:35 +0530

    [ashutosh] add:Insert Between Linked List

--- DataStructures/DataStructures/LinkedList.cs
+++ DataStructures/DataStructures/LinkedList.cs
@@ -1,5 +1,4 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -39,32 +38,6 @@ namespace DataStructures
                 Console.WriteLine(temp.data + " ");
                 temp = temp.next;
             }
-        }
-        internal Node InsertAtParticularPosition(int position, int data)
-        {
-            Node newestNode = new Node(data);
-            if (this.head == null)
-            {
-                return newestNode;
-            }
-            if (position == 0)
-            {
-                newestNode.next = this.head;
-                this.head = newestNode;
-                return this.head;
-            }
-            Node prev = null;
-            Node current = this.head;
-            int count = 0;
-            while (current != null && count < position)
-            {
-                prev = current;
-                current = current.next;
-                count++;
-            }
-            newestNode.next = prev.next;
-            prev.next = newestNode;
-            return this.head;
         }
     }
 }
